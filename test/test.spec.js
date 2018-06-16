1efb66865265bc47dc1a5e0bed527cc45713f295
Daniel Clarke <clarkit@gmail.com>
2018-06-16 02:11:25 +0100

    After github account rename

--- test/test.spec.js
+++ test/test.spec.js
@@ -4,5 +4,6 @@ describe("A Sample Test Suite", () => {
   it("Evaluates to true", () => {
       expect(true).toBeTruthy();
       expect(false).toBeFalsy();
+      expect(1e3).toEqual(1000);
   });
 });
