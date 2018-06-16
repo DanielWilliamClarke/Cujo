cc002ff7473162e71376bd212ca6cb9ae1ac85f5
Daniel Clarke <clarkit@gmail.com>
2018-06-17 00:22:10 +0100

    Docker should still be working

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-console.log(__dirname)
+console.log((() => { return __dirname })());
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname +'./../../dist/')); //serves the index.html
 app.listen(port); //listens on port 3000 -> http://localhost:3000/ 
+
+
