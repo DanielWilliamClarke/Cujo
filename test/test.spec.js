cd9b8d6278fd6299088b558d82a08feb1249b751
Daniel Clarke <clarkit@gmail.com>
2018-06-16 01:49:14 +0100

    karma test runner and code coverage in place

---
+++ test/test.spec.js
@@ -0,0 +1,8 @@
+import Input from "../src/client/components/presentational/Input";
+
+describe("A Sample Test Suite", () => {
+  it("Evaluates to true", () => {
+      expect(true).toBeTruthy();
+      expect(false).toBeFalsy();
+  });
+});
