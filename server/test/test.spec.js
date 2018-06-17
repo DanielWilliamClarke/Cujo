16d156ee7c2d82037903078d3e1531086a5e04b7
Daniel Clarke <clarkit@gmail.com>
2018-06-17 19:44:47 +0100

    test runner now in place for server app

---
+++ server/test/test.spec.js
@@ -0,0 +1,8 @@
+
+describe("A Sample Test Suite", () => {
+  it("Evaluates to true", () => {
+      expect(true).toBeTruthy();
+      expect(false).toBeFalsy();
+      expect(1e3).toEqual(1000);
+  });
+});
