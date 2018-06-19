637358e94ee559a1ce5e597682d376b4fd65f088
Daniel Clarke <DClarke5@slb.com>
2018-06-19 15:58:22 +0100

    got markdown rendering and markdown styling somewhat in place

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,8 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import FormContainer from "./components/container/FormContainer";
 
 console.log(process.env.PORT); 
 
+const wrapper = document.getElementById("create-article-form");
+wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
