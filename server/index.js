2025bfab6e2da43bc9dc382035818e0cbb6839c7
Daniel Clarke <clarkit@gmail.com>
2018-06-23 16:01:14 +0100

    moved mongo config and auth to env file, working fror dev prod and docker envs

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
+const dotenv = require("dotenv");
+const path = require("path");
+if (process.env.NODE_ENV !== 'prod' || process.env.NODE_ENV !== 'docker') {
+    dotenv.config({path: path.resolve(__dirname, 'server.env')});
+}
+
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-
 const MongoSetup = require("./src/connection/mongo_setup");
 
 const schema = require("./src/graphql");
@@ -11,10 +16,13 @@ app.use("/", graphqlHTTP({
   graphiql: true
 }));
 
-const mongoPort = process.env.MONGOPORT || 27017;
-const mongoHost = process.env.MONGOHOST || "localhost";
-const mongoUrl = `mongodb://external:readOnly@${mongoHost}:${mongoPort}/moderncvapp`;
-const mongoSetup = new MongoSetup(mongoUrl);
+const mongoSetup = new MongoSetup({
+  host: process.env.MONGOHOST,
+  port: process.env.MONGOPORT,
+  user: process.env.MONGOUSER,
+  pass: process.env.MONGOPASS,
+  collection: process.env.MONGOCOLLECTION
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
--- server/src/connection/mongo_setup.js
+++ server/src/connection/mongo_setup.js
@@ -1,27 +1,31 @@
 const mongoose = require("mongoose");
 
+const ConstructURI = config => `mongodb://${config.user}:${config.pass}@${config.host}:${config.port}/${config.collection}`
+
 module.exports = class MongoSetup {
 
-  constructor(mongoUrl) {
-    this.url = mongoUrl;
+  constructor(mongoConfig) {
+    this.config = mongoConfig;
+    this.uri = ConstructURI(this.config);
+    this.hostFragment = `${this.config.host}:${this.config.port}`;
     this.connect = this.connect.bind(this);
     this.setupHandlers.apply(this);
   }
 
   connect() {
-    console.log(`attempting connection on to mongo on ${this.url}`);
-    mongoose.connect(this.url).catch(() => {
-      console.log(`Failed to connect to mongo on ${this.url}`)
+    console.log(`attempting connection on to mongo on ${this.hostFragment}`);
+    mongoose.connect(this.uri).catch(() => {
+      console.log(`Failed to connect to mongo on ${this.hostFragment}`)
     })
   }
 
   setupHandlers () {
     mongoose.connection.on("connected", () => {
-      console.log(`Successfully Connected to Mongo on ${this.url}`);
+      console.log(`Successfully Connected to Mongo on ${this.hostFragment}`);
     });
 
     mongoose.connection.on('disconnected', () => {  
-      console.log(`Disconnecting from ${this.url}`); 
+      console.log(`Disconnecting from ${this.hostFragment}`); 
     });
 
     mongoose.connection.on("error", (error) => {
@@ -31,4 +35,6 @@ module.exports = class MongoSetup {
       }
     });
   }
+
+
 }
