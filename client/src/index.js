227c6165344ab7a51dac301cf77ef83850fb9e88
Daniel Clarke <DClarke5@slb.com>
2019-02-07 13:11:58 +0000

    adding files

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import FormContainer from "./components/container/FormContainer";
-
-console.log(process.env.PORT); 
-
-const wrapper = document.getElementById("create-article-form");
-wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
