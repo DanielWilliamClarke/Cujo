7f747ea7289502f1791c03df487db8d11b53b2c5
Daniel William Clarke <DClarke5@slb.com>
2020-05-11 22:12:54 +0100

    replaced client with create-react app

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import FormContainer from "./components/container/FormContainer";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-console.log(process.env.PORT); 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const wrapper = document.getElementById("create-article-form");
-wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
