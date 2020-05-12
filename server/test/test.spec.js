24502f484f001ea03ad377b7177398f964ff9055
Daniel William Clarke <DClarke5@slb.com>
2020-05-12 14:03:25 +0100

    trashed server code

--- server/test/test.spec.js
+++
@@ -1,8 +0,0 @@
-
-describe("A Sample Test Suite", () => {
-  it("Evaluates to true", () => {
-      expect(true).toBeTruthy();
-      expect(false).toBeFalsy();
-      expect(1e3).toEqual(1000);
-  });
-});
