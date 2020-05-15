b35ace38fc38779d9b721a73e1075067f0d8661a
Daniel William Clarke <DClarke5@slb.com>
2020-05-15 21:02:03 +0100

    client can now pick up data from server (albeit hardcorded wise)

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+axios.get("http://localhost:5000/api/v1/cv")
+  .then(res => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App cv={res.data} />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
