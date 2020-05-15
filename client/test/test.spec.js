1be7ec9ec2a6710cbf87c1294a2463fc47f27079
Daniel William Clarke <DClarke5@slb.com>
2020-05-15 21:02:03 +0100

    client can now pick up data from server (albeit hardcorded wise)

--- client/test/test.spec.js
+++
@@ -1,9 +0,0 @@
-import Input from "../src/components/presentational/Input";
-
-describe("A Sample Test Suite", () => {
-  it("Evaluates to true", () => {
-      expect(true).toBeTruthy();
-      expect(false).toBeFalsy();
-      expect(1e3).toEqual(1000);
-  });
-});
