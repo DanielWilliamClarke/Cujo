084b2dc2172379bd31bfc957e9a8568bcf4f4465
Daniel Clarke <Clarkit@gmail.com>
2021-02-10 18:07:16 +0000

    made a start on client UI

---
+++ client/src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import "./index.css";
+import App from "./App";
+
+import { CV } from "./CVModel";
+
+axios.get("/api/v1/cv").then((res: { data: CV }): void => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App cv={res.data} />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
