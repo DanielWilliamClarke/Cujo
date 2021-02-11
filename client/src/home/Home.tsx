6c6b9f427a6ae69197ca22af8403b8ba3af9a5f1
Daniel Clarke <Clarkit@gmail.com>
2021-02-11 15:56:51 +0000

    back stretch and sticky nav

--- client/src/home/Home.tsx
+++ client/src/home/Home.tsx
@@ -3,8 +3,26 @@ import { CVProps } from "../model/CV";
 
 import "./Home.css";
 import headshot from "../assets/headshot.jpg";
+import { LoremIpsum } from "lorem-ipsum";
 
 export class Home extends Component<CVProps> {
+  private lorem: LoremIpsum;
+
+  constructor(props: CVProps) {
+    super(props);
+    
+    this.lorem = new LoremIpsum({
+      sentencesPerParagraph: {
+        max: 8,
+        min: 4,
+      },
+      wordsPerSentence: {
+        max: 16,
+        min: 4,
+      },
+    });
+  }
+
   render(): JSX.Element {
     return (
       <div className="Home">
@@ -18,6 +36,7 @@ export class Home extends Component<CVProps> {
             <div className="Card-title">
               Hi, name is {this.props.cv.basics.name}!
             </div>
+            {this.lorem.generateParagraphs(1000)}
           </div>
         </div>
       </div>
