6ce7ed90d0e07333baa1ab9da2ff727ee92bb03d
DanielWilliamClarke <clarkit@gmail.com>
2021-02-14 11:32:13 +0000

    Commiting blog code

---
+++ client/src/components/blog/Blog.tsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import WPAPI from "wpapi";
+import Interweave from "interweave";
+
+import { BlogPost } from "./BlogModel";
+
+import "../../shared/Section.css";
+import "./Blog.css";
+
+type BlogState = {
+  posts: BlogPost[];
+};
+
+export class Blog extends Component<{}, BlogState> {
+  private wp: WPAPI;
+
+  constructor(props: {}) {
+    super(props);
+    this.wp = new WPAPI({
+      endpoint: "http://localhost:8000/wp-json",
+    });
+  }
+
+  setBlogState(posts: BlogPost[]): void {
+    this.setState({ posts: posts });
+  }
+
+  componentWillMount(): void {
+    this.setBlogState([]);
+    this.wp
+      .posts()
+      .then(this.setBlogState.bind(this))
+      .catch((err) => console.log(err));
+  }
+
+  render(): JSX.Element {
+    return (
+      <Container>
+        {this.state.posts.map((post) => (
+          <section className="Section">
+            <Row className="Section-content">
+              <Col className="Centered">
+                <h2>{post.title.rendered}</h2>
+                <Interweave content={post.content.rendered} />
+              </Col>
+            </Row>
+          </section>
+        ))}
+      </Container>
+    );
+  }
+}
