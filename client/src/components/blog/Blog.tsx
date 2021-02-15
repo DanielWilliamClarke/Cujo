7da9c496c52f5390772b5c15dfaa5d93241005b1
Daniel Clarke <Clarkit@gmail.com>
2021-02-15 17:24:46 +0000

    fleshed out blog, now has both media and posts some blog styling

--- client/src/components/blog/Blog.tsx
+++ client/src/components/blog/Blog.tsx
@@ -1,17 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import WPAPI from "wpapi";
-import Interweave from "interweave";
 
-import { BlogPost } from "./BlogModel";
+import { Post } from "./BlogPostModel";
+import { Media } from "./BlogMediaModel";
 
 import "../../shared/Section.scss";
 import "./Blog.scss";
+import "highlight.js/scss/tomorrow-night-eighties.scss";
+
+interface BlogPost {
+  post: Post;
+  media: Media;
+}
 
 type BlogState = {
-  posts: BlogPost[];
+  blog: BlogPost[];
 };
-
 export class Blog extends Component<{}, BlogState> {
   private wp: WPAPI;
 
@@ -22,34 +27,62 @@ export class Blog extends Component<{}, BlogState> {
     });
   }
 
-  setBlogState(posts: BlogPost[]): void {
-    this.setState({ posts: posts });
+  correlate([posts, media]: [Post[], Media[]]): void {
+    const blog: BlogPost[] = posts.map(
+      (p: Post): BlogPost => {
+        return {
+          post: p,
+          media: media.find((m: Media): boolean => m.post === p.id),
+        } as BlogPost;
+      }
+    );
+
+    this.setState({ blog });
   }
 
   componentWillMount(): void {
-    this.setBlogState([]);
-    this.wp
-      .posts()
-      .then(this.setBlogState.bind(this))
+    this.setState({ blog: [] });
+    Promise.all([this.wp.posts(), this.wp.media()])
+      .then(this.correlate.bind(this))
       .catch((err) => console.log(err));
   }
 
   render(): JSX.Element {
     return (
-      <Container>
-        {this.state.posts.map((post) => (
-          <section className="Section">
-            <Row className="Section-content">
-              <Col className="Centered">
-                <h2>{post.title.rendered}</h2>
-                <div className="Centered Line"></div>
-                <Interweave content={post.content.rendered} />
-                <div className="Centered Short-line"></div>
-              </Col>
-            </Row>
+      <Fragment>
+        {this.state.blog.map((b) => (
+          <section className="Section Blog">
+            <Container>
+              <Row className="Section-content">
+                <Col className="Centered">
+                  <h2>{b.post.title.rendered}</h2>
+                  <div className="Centered Line"></div>
+                </Col>
+              </Row>
+
+              {b.media && (
+                <Row className="Section-content">
+                  <Col className="Centered">
+                    <img src={b.media.source_url} alt="not found..." />
+                  </Col>
+                </Row>
+              )}
+
+              <Row className="Section-content Blog-content">
+                <Col className="Centered">
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: b.post.content.rendered,
+                    }}
+                  ></div>
+                </Col>
+              </Row>
+
+              <div className="Centered Short-line"></div>
+            </Container>
           </section>
         ))}
-      </Container>
+      </Fragment>
     );
   }
 }
