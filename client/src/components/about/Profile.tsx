577045f9fd5b3aaba085e31a3342e762809418a0
Daniel Clarke <Clarkit@gmail.com>
2021-02-16 21:02:34 +0000

    Updated sharepanel and added to profile also

--- client/src/components/about/Profile.tsx
+++ client/src/components/about/Profile.tsx
@@ -1,5 +1,7 @@
+
 import { Component } from "react";
 
+import { SharePanel } from "../nav/SharePanel";
 import { About } from "./About";
 import { Experience } from "./Experience";
 import { Technical } from "./Technical";
@@ -13,8 +15,16 @@ export class Profile extends Component<CVProps> {
   render(): JSX.Element {
     return (
       <div>
+        <SharePanel
+          url={window.location.href}
+          body="Software Engineer Portfolio and Blog"
+          hashtag="DWCPortfolio"
+        />
         <Fade bottom>
-          <About basics={this.props.cv.basics} interests={this.props.cv.interests} />
+          <About
+            basics={this.props.cv.basics}
+            interests={this.props.cv.interests}
+          />
         </Fade>
         <Fade bottom>
           <Technical skills={this.props.cv.skills} />
