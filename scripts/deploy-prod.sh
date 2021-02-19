2e987e5cfa3d4697be9de6b7938755926eedecb8
Daniel Clarke <Clarkit@gmail.com>
2021-02-19 23:56:26 +0000

    deploy now deploys to background

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -4,4 +4,4 @@ ENVFILE=/opt/Cujo/.env
 COMPOSEFILE=prod.compose.yaml
 
 # Deploy
-sudo docker-compose --env-file $ENVFILE -f $COMPOSEFILE up
+sudo docker-compose --env-file $ENVFILE -f $COMPOSEFILE up -d
