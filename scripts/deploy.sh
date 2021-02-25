8b38f4912872e886cd577d15ce8ab3d57b152b40
Daniel Clarke <Clarkit@gmail.com>
2021-02-25 16:25:31 +0000

    made background configurable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 DEPLOYMENT=$1
+BG=$2
 
 ENVFILE=/opt/Cujo/.env
 COMPOSEFILE=$DEPLOYMENT.compose.yaml
 
 # Deploy
-docker-compose --env-file $ENVFILE -f $COMPOSEFILE up -d
+docker-compose --env-file $ENVFILE -f $COMPOSEFILE up $BG
