bc7a4a4c7ea2e139e54113b018d3cb6c6012f3c2
Daniel Clarke <Clarkit@gmail.com>
2021-06-02 17:22:52 +0100

    Implemented p5 based sketch animations for backstretch instead of static image

---
+++ portfolio/src/sketches/hex.ts
@@ -0,0 +1,179 @@
+import p5 from "p5";
+
+class HSLA {
+    constructor(public h: number = 0, public s: number = 0, public b: number = 0, public a: number = 1)  {}
+    toString() {
+        return `hsla(${this.h}, ${this.s}%, ${this.b}%, ${this.a})`;
+    }
+}
+
+// Global tick value
+let TICK = 0;
+const edges = 6;
+
+const options = {
+    len: 20,
+    count: 50,
+    baseTime: 10,
+    addedTime: 10,
+    dieChance: .02,
+    spawnChance: 1,
+    sparkChance: .1,
+    sparkDist: 10,
+    sparkSize: 2,
+    color: new HSLA(),
+    baseLight: 50,
+    addedLight: 10, // [50-10,50+10]
+    shadowToTimePropMult: 6,
+    baseLightInputMultiplier: .02,
+    addedLightInputMultiplier: .02,
+    repaintAlpha: .04,
+    hueChange: .5
+};
+
+class HexLine {
+
+    constructor(
+        public dieX: number = 0,
+        public dieY: number = 0,
+        public x: number = 0,
+        public y: number = 0,
+        public addedX: number = 0,
+        public addedY: number = 0,
+        public rad: number = 0,
+        public baseRad = Math.PI * 2 / edges,
+        public lightInputMultiplier: number = 0,
+        public color: HSLA = new HSLA(),
+        public cumulativeTime: number = 0,
+        public time: number = 0,
+        public targetTime: number = 0) 
+    {
+        this.reset();
+    }
+
+    reset(): void {
+        this.x = 0;
+        this.y = 0;
+        this.addedX = 0;
+        this.addedY = 0;
+        this.rad = 0;
+        this.cumulativeTime = 0;
+
+        this.lightInputMultiplier = options.baseLightInputMultiplier + options.addedLightInputMultiplier * Math.random();
+        this.color = new HSLA(TICK * options.hueChange, 100, 100);
+
+        this.beginPhase();
+    }
+
+    beginPhase(): void {
+        this.x += this.addedX;
+        this.y += this.addedY;
+
+        this.time = 0;
+        this.targetTime = (options.baseTime + options.addedTime * Math.random()) | 0;
+
+        this.rad += this.baseRad * (Math.random() < .5 ? 1 : -1);
+        this.addedX = Math.cos(this.rad);
+        this.addedY = Math.sin(this.rad);
+
+        if (Math.random() < options.dieChance || 
+            this.x > this.dieX || 
+            this.x < -this.dieX || 
+            this.y > this.dieY || 
+            this.y < -this.dieY) {
+            this.reset();
+        }     
+    }
+
+    step(p: p5, ctx: any, centerX: number, centerY: number): void {
+
+        ++this.time;
+        ++this.cumulativeTime;
+
+        if (this.time >= this.targetTime) {
+            this.beginPhase();
+        }
+    
+        const prop = this.time / this.targetTime;
+        const wave = Math.sin(prop * Math.PI / 2)
+        const x = this.addedX * wave;
+        const y = this.addedY * wave;
+
+        this.color.b = options.baseLight + options.addedLight * Math.sin(this.cumulativeTime * this.lightInputMultiplier);
+
+        ctx.shadowBlur = prop * options.shadowToTimePropMult;
+        ctx.shadowColor = this.color.toString();
+        p.fill(this.color.h, this.color.s, this.color.b);
+        p.rect(
+            centerX + (this.x + x) * options.len,
+            centerY + (this.y + y) * options.len,
+            2, 2);
+
+        if (Math.random() < options.sparkChance) {
+            p.rect(
+                centerX + (this.x + x) * options.len + Math.random() * options.sparkDist * (Math.random() < .5 ? 1 : -1) - options.sparkSize / 2,
+                centerY + (this.y + y) * options.len + Math.random() * options.sparkDist * (Math.random() < .5 ? 1 : -1) - options.sparkSize / 2,
+                2, 2);
+        }     
+    }
+}
+
+const sketch = (p: p5): void => {
+
+    const lines: HexLine[] = [];
+  
+    let screenWidth = 0;
+    let screenHeight = 0;
+    let centerX = 0;
+    let centerY = 0;
+    let dieX = 0;
+    let dieY = 0;
+
+    let ctx: any;
+
+    p.setup = (): void => {
+        p.frameRate(60);
+        p.colorMode(p.HSB, 100);
+        p.createCanvas(window.innerWidth, window.innerHeight);
+
+        const canvas = document.getElementById('defaultCanvas0') as any;
+        ctx = canvas.getContext('2d');
+
+        screenWidth = p.width = window.innerWidth;
+        screenHeight = p.height = window.innerHeight;
+        centerX = screenWidth / 2;
+        centerY = screenHeight / 2;
+        dieX = screenWidth / 2 / options.len;
+        dieY = screenHeight / 2 / options.len;
+    };
+
+    p.windowResized = (): void => {
+        p.resizeCanvas(window.innerWidth, window.innerHeight);
+        screenWidth = p.width = window.innerWidth;
+        screenHeight = p.height = window.innerHeight;
+        centerX = screenWidth / 2;
+        centerY = screenHeight / 2;
+        dieX = screenWidth / 2 / options.len;
+        dieY = screenHeight / 2 / options.len;
+    };
+
+    p.draw = (): void => {
+
+        ++TICK;
+
+        ctx.globalCompositeOperation = 'source-over';
+        ctx.shadowBlur = 0;
+        ctx.fillStyle = `rgba(0,0,0,${options.repaintAlpha})`;
+        ctx.fillRect( 0, 0, screenWidth, screenHeight );
+        ctx.globalCompositeOperation = 'lighter';
+
+        if (lines.length < options.count &&
+            Math.random() < options.spawnChance) {
+            lines.push(new HexLine(dieX, dieY));
+        }
+
+        lines.forEach((line: HexLine) => line.step(p, ctx, centerX, centerY));
+    };
+};
+
+export default sketch;
---
+++ portfolio/src/sketches/waves.ts
@@ -0,0 +1,137 @@
+import p5 from "p5";
+
+const SimplexNoise = require('simplex-noise');
+
+class HSLA {
+    constructor(public h: number = 0, public s: number = 0, public b: number = 0, public a: number = 0)  {}
+}
+
+class Particle {
+    public pastX: number = 0;
+    public pastY: number = 0;
+
+    constructor(public x: number = 0, public y: number = 0, public color: HSLA = new HSLA()) {
+    }
+
+    init(hueBase: number, screenWidth: number, screenHeight: number, centerX: number, centerY: number): void {
+        this.x = this.pastX = screenWidth * Math.random();
+        this.y = this.pastY = screenHeight * Math.random();
+        this.color.h = Math.atan2(this.y - centerY, this.x - centerX) * (180 / Math.PI);
+        this.color.s = 100;
+        this.color.b = 100;
+        this.color.a = 0.95;
+    }
+
+    persistPosition(): void {
+        this.pastX = this.x;
+        this.pastY = this.y;
+    }
+
+    integratePosition(x: number, y: number): void {
+        this.x += x;
+        this.y += y;
+    } 
+}
+
+class NoiseGenerator {
+
+    constructor(private sn: any) {
+    }
+
+    getNoise(x: number, y: number, z: number): number {
+        var octaves = 12,
+            fallout = 0.3,
+            amp = 1, f = 1, sum = 0,
+            i;
+    
+        for (i = 0; i < octaves; ++i) {
+            amp *= fallout;
+            sum += amp * (this.sn.noise3D(x * f, y * f, z * f) + 1) * 0.5;
+            f *= 2;
+        }
+    
+        return sum;
+    }
+
+    refresh(sn: any) {
+        this.sn = sn;
+    }
+}
+
+const sketch = (p: p5): void => {
+
+    const noiseGenerator = new NoiseGenerator(new SimplexNoise());
+    const totalParticles = 1000;
+    const particles: Particle[] = [];
+    let screenWidth = 0;
+    let screenHeight = 0;    
+    let centerX = 0;
+    let centerY = 0;
+    let hueBase = 0;
+
+    const step = 10;
+    const base = 1000;
+    const zInc = 0.001;
+    let zOff = 0;
+
+    p.setup = (): void => {
+        p.frameRate(60);
+        p.colorMode(p.HSB, 100);
+
+        p.createCanvas(window.innerWidth, window.innerHeight);
+        screenWidth  = p.width  = window.innerWidth;
+        screenHeight = p.height = window.innerHeight;
+        centerX = screenWidth / 2;
+        centerY = screenHeight / 2;
+
+        for (var i = 0, len = totalParticles; i < len; i++) {
+            const p = new Particle();
+            p.init(hueBase, screenWidth, screenHeight, centerX, centerY);
+            particles.push(p)
+        }
+    };
+
+    p.windowResized = (): void => {
+        p.resizeCanvas(window.innerWidth, window.innerHeight);
+        screenWidth  = p.width  = window.innerWidth;
+        screenHeight = p.height = window.innerHeight;
+        centerX = window.innerWidth / 2;
+        centerY = window.innerHeight / 2;
+        noiseGenerator.refresh(new SimplexNoise());
+    }
+
+    p.draw = (): void => {
+
+        particles.forEach((particle: Particle): void => {
+            particle.persistPosition();
+
+            const angle = Math.PI * 6 * noiseGenerator.getNoise(
+                particle.x / base * 1.75, 
+                particle.y / base * 1.75,
+                zOff);
+
+            particle.integratePosition(
+                Math.cos(angle) * step,
+                Math.sin(angle) * step);
+
+            if (particle.color.a < 1) {
+                particle.color.a += 0.003;   
+            }
+
+            // draw pariticle
+            p.fill(particle.color.h, particle.color.s, particle.color.b, particle.color.a);
+            p.stroke(particle.color.h, particle.color.s, particle.color.b, particle.color.a);
+            p.line(particle.pastX, particle.pastY, particle.x, particle.y);
+            
+            if ((particle.x < 0 || particle.x > window.innerWidth) ||
+                 (particle.y < 0 || particle.y > window.innerHeight)) {
+                particle.init(hueBase, screenWidth, screenHeight, centerX, centerY);
+            }
+        });
+        
+        zOff += zInc;
+        hueBase += 0.1;
+    };
+};
+
+export default sketch;
