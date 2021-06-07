53cb454b1ddd9b8cad72f5317e41925ebcc6abc7
Daniel Clarke <Clarkit@gmail.com>
2021-06-07 15:03:27 +0100

    Updates to phylo sketch

--- portfolio/src/sketches/phylotaxis.ts
+++ portfolio/src/sketches/phylotaxis.ts
@@ -3,7 +3,7 @@ import p5 from "p5";
 const sketch = (p: p5): void => {
 
     let n = 0;
-    let c = 10;
+    let c = 8;
     let start = 0;
     let growth: boolean = true;
 
@@ -29,38 +29,37 @@ const sketch = (p: p5): void => {
 
         for (var i = 0; i < n; i++) {
             // vary magic angle
-            let ma =  p.sin(i);
+            let ma = p.sin(i);
             ma = p.map(ma, -1, 1, 137.3, 137.6);
-           
+
             // Calculate angle and radis
             const a = i * ma;
             const r = c * p.sqrt(i);
-
             // const x =  r * p.cos(a);
             // const y = r * p.sin(a);
 
             // Polar to cartesian converstion with some wiggle
             const lerpX1 = r * p.cos(a);
-            const lerpX2 = r * p.cos((i + 1) * magicAngle)
+            const lerpX2 = r * p.cos((i + 2) * magicAngle)
             const lerpY1 = r * p.sin(a);
-            const lerpY2 = r * p.sin((i + 1) * magicAngle);
+            const lerpY2 = r * p.sin((i + 2) * magicAngle);
 
             const x = p.lerp(lerpX1, lerpX2, p.sin(p.millis() / 100))
             const y = p.lerp(lerpY1, lerpY2, p.cos(p.millis() / 100))
 
             // Calculate color hue
             let hu = p.sin(start - i * 0.5);
-            hu = p.map(hu, -1, 1, 0, 360); 
+            hu = p.map(hu, -1, 1, 0, 360);
 
+            // Here I want to try to keep particle max size consistent accross multiple device widths
+            const maxSize = p.map(p.width / 2, 375, 1440, 10, 15);
             // Calcualte particle size based on radius
-            const d = p.map(r, 0, p.height / 2, 0.5, 15);
-            // Calculate color alpha based on size
-            const alpha = p.map(d, 1, 15, 1, 0.8);
+            const d = p.map(r, 0, p.height / 2, 0.5, maxSize);
 
             // Draw
-            p.fill(hu, 255, 255, alpha);
-            p.noStroke();           
-            p.ellipse(x, y, d, d, 5); 
+            p.fill(hu, 150, 255);
+            p.noStroke();
+            p.ellipse(x, y, d, d, 5);
         }
 
         // Toggle growth / skrinking
@@ -69,7 +68,7 @@ const sketch = (p: p5): void => {
         }
         n += growth ? increment : -increment;
 
-        start += 1;
+        start += 5;
     }
 };
 
