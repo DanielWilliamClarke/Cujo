f7e034165b1e38c5b041fd2874f0b08091485550
Daniel Clarke <Clarkit@gmail.com>
2021-06-16 12:05:22 +0100

    Now call rust service on page start up, the service now passes the cv data service is not exposed externally and is only accessible via the portfolio

--- portfolio/src/index.tsx
+++ portfolio/src/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios, { AxiosResponse } from "axios";
+
 import "./index.css";
 import { App } from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import data from "./assets/data.json";
 import { CV } from "./model/CVModel";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App cv={data as CV} />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+axios.get('/api/cv')
+  .then((response: AxiosResponse) => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App cv={response.data as CV} />
+      </React.StrictMode>,
+      document.getElementById("root")
+    );
+  });
+
+
