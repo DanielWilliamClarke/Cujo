318ecf63fd2e4cb5bbbd47b04501c07749025e3c
DanielWilliamClarke <clarkit@gmail.com>
2021-06-19 23:39:52 +0100

    simplified and cleanup nav it actually now works properly

--- portfolio/src/components/nav/NavPanel.tsx
+++ portfolio/src/components/nav/NavPanel.tsx
@@ -1,49 +1,39 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import { NavHashLink } from "react-router-hash-link";
 
 import "./NavPanel.scss";
-import logo from "../../assets/logo_blue.png";
 
-interface StyleProps {
-  style: React.CSSProperties;
-}
-
-export class NavPanel extends Component<StyleProps> {
+export class NavPanel extends Component {
   render(): JSX.Element {
     return (
-      <nav style={this.props.style} className="nav-panel">
-        <div className="nav-container nav-logo">
-          <img src={logo} alt="logo" />
-        </div>
-        <Container>
-          <Row className="nav-container">
-            {[
-              "about",
-              "experience",
-              "education",
-              "projects",
-              "skills",
-              "contact",
-            ].map((hash: string): JSX.Element => this.createLink("", hash))}
-            {this.createLink("blog", "blog")}
-          </Row>
-        </Container>
-      </nav>
+      <Navbar
+        sticky="top"
+        bg="dark"
+        variant="dark"
+        className="justify-content-center"
+      >
+        <Nav
+          justify
+          variant="pills"
+          navbarScroll
+          style={{ textTransform: "capitalize" }}
+        >
+          {[
+            "about",
+            "experience",
+            "education",
+            "projects",
+            "skills",
+            "contact",
+          ].map((hash: string): JSX.Element => this.createLink("/", hash))}
+          {this.createLink("/blog", "blog")}
+        </Nav>
+      </Navbar>
     );
   }
 
   private createLink(link: string, hash: string): JSX.Element {
-    return (
-      <Col md="auto" className="nav-button option">
-        <NavHashLink
-          activeClassName="nav-active"
-          smooth
-          to={`/${link}#${hash}`}
-        >
-          {hash}
-        </NavHashLink>
-      </Col>
-    );
+    return <Nav.Link href={`${link}#${hash}`}>{hash}</Nav.Link>;
   }
 }
