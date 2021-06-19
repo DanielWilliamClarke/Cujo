4411280c4a7c9c8aa2773000fc7c5c888c5b3c3f
DanielWilliamClarke <clarkit@gmail.com>
2021-06-19 15:59:22 +0100

    fix index.scss inclusion

--- portfolio/src/index.tsx
+++ portfolio/src/index.tsx
@@ -2,21 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios, { AxiosResponse } from "axios";
 
-import "./index.css";
+import "./index.scss";
 import { App } from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { CV } from "./model/CVModel";
 
-axios.get('/api/cv')
-  .then((response: AxiosResponse) => {
-    ReactDOM.render(
-      <React.StrictMode>
-        <App cv={response.data as CV} />
-      </React.StrictMode>,
-      document.getElementById("root")
-    );
-  });
-
-
+axios.get("/api/cv").then((response: AxiosResponse) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App cv={response.data as CV} />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
