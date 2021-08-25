cc74a8e2fdfe6bfbf28e829473b28e11358b1781
DanielWilliamClarke <clarkit@gmail.com>
2021-08-25 23:18:58 +0100

    contact service now no longer needs callback

--- portfolio/src/components/backstretch/Copyright.tsx
+++ portfolio/src/components/backstretch/Copyright.tsx
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 
 import { Container, Row, Col } from "react-bootstrap";
 
-import "./Copyright.scss"
-
+import "./Copyright.scss";
 export class Copyright extends Component<{}> {
   render(): JSX.Element {
     return (
