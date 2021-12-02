2cc5d0edb2f330bc18fb41abf65eb05b4b582636
Daniel Clarke <Clarkit@gmail.com>
2021-12-02 13:59:15 +0000

    made about section animated

--- portfolio/src/components/profile/About.tsx
+++ portfolio/src/components/profile/About.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Row, Col, Carousel } from "react-bootstrap";
 import ReactMarkdown from "react-markdown";
 import breaks from "remark-breaks";
+import { Fade, Zoom } from "react-awesome-reveal";
 
 import { GiDiceTwentyFacesTwenty } from "react-icons/gi";
 
@@ -27,56 +28,64 @@ export class About extends Component<AboutProps> {
         icon={GiDiceTwentyFacesTwenty}>
 
         <Row className="section-content">
-          <Carousel
-            indicators
-            wrap={true}
-            slide={true}
-            nextLabel={""}
-            prevLabel={""}
-          >
-            {this.props.basics.images.map((src: string) => (
-              <Carousel.Item>
-                <DynamicImage
-                  image={src}
-                  alt="Image not found!"
-                  className="headshot"
-                />
-              </Carousel.Item>
-            ))}
-          </Carousel>
-        </Row>
-
-        <Row className="section-content">
-          <Col className="text-column">
-            <h4>A little about me!</h4>
-            <ReactMarkdown
-              children={this.props.basics.summary}
-              remarkPlugins={[breaks]}
-            />
-          </Col>
-
-          <Col className="text-column">
-            <h4>My Interests</h4>
-            <ReactMarkdown
-              children={this.props.interests.summary}
-              remarkPlugins={[breaks]}
-            />
-            <ul className="interests">
-              {this.props.interests.list.map((interest: string) => (
-                <li>
-                  <ReactMarkdown children={interest} />
-                </li>
+          <Zoom triggerOnce damping={0.01}>
+            <Carousel
+              indicators
+              wrap={true}
+              slide={true}
+              nextLabel={""}
+              prevLabel={""}
+            >
+              {this.props.basics.images.map((src: string) => (
+                <Carousel.Item>
+                  <DynamicImage
+                    image={src}
+                    alt="Image not found!"
+                    className="headshot"
+                  />
+                </Carousel.Item>
               ))}
-            </ul>
+            </Carousel>
+          </Zoom>
+        </Row>
+
+        <Row className="section-content">
+          <Col className="text-column">
+            <Fade triggerOnce direction="left">
+              <h4>A little about me!</h4>
+              <ReactMarkdown
+                children={this.props.basics.summary}
+                remarkPlugins={[breaks]}
+              />
+            </Fade>
+          </Col>
+
+          <Col className="text-column">
+            <Fade triggerOnce direction="right">
+              <h4>My Interests</h4>
+              <ReactMarkdown
+                children={this.props.interests.summary}
+                remarkPlugins={[breaks]}
+              />
+              <ul className="interests">
+                {this.props.interests.list.map((interest: string) => (
+                  <li>
+                    <ReactMarkdown children={interest} />
+                  </li>
+                ))}
+              </ul>
+            </Fade>
           </Col>
         </Row>
 
         <Row className="section-content">
-          <Col className="mailto">
-            <a href={`mailto:${this.props.basics.email}`}>
-              {this.props.basics.email}
-            </a>
-          </Col>
+          <Zoom triggerOnce damping={0.01}>
+            <Col className="mailto">
+              <a href={`mailto:${this.props.basics.email}`}>
+                {this.props.basics.email}
+              </a>
+            </Col>
+          </Zoom>
         </Row>
 
       </Section>
