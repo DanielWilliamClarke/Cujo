3a56b71416769a66663d9c3d007ffe1574a81a42
DanielWilliamClarke <clarkit@gmail.com>
2022-03-15 15:10:00 +0000

    update readme + refactor

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,25 +1,23 @@
-import { Component } from "react";
 import { resolve } from "inversify-react";
+import { Component } from "react";
 import {
-  Switch,
   Route,
   RouteComponentProps,
+  Switch,
   withRouter,
 } from "react-router-dom";
-
-import { ICujoService } from "./services/CujoService";
-import { SketchBackstretch } from "./components/backstretch/SketchBackstretch";
-import { CV } from "./model/CVModel";
-import { ContentfulEntries } from "./model/ContentfulEntries";
-import NavPanel from "./components/nav/NavPanel";
+import "./App.scss";
 import { Copyright } from "./components/backstretch/Copyright";
-import { Profile } from "./components/Profile";
+import { SketchBackstretch } from "./components/backstretch/SketchBackstretch";
 import { Blog } from "./components/blog/Blog";
 import { BlogPost } from "./components/blog/BlogPost";
 import { Contact } from "./components/contact/Contact";
+import NavPanel from "./components/nav/NavPanel";
 import { SharePanel } from "./components/nav/SharePanel";
-
-import "./App.scss";
+import { Profile } from "./components/Profile";
+import { ContentfulEntries } from "./model/ContentfulEntries";
+import { CV } from "./model/CVModel";
+import { ICujoService } from "./services/CujoService";
 
 export type AppState = {
   cv: CV | undefined;
@@ -30,8 +28,8 @@ type BlogRouteParams = { id: string };
 
 class App extends Component<RouteComponentProps, AppState> {
   @resolve("CujoService") private readonly cujoService!: ICujoService;
+
   async componentWillMount() {
-    this.setState({ cv: undefined, blog: undefined });
     this.setState({
       cv: await this.cujoService.FetchCV(),
       blog: await this.cujoService.FetchBlogPosts(),
@@ -39,44 +37,43 @@ class App extends Component<RouteComponentProps, AppState> {
   }
 
   render(): JSX.Element {
+    if (!this.state?.cv) {
+      return <></>;
+    }
+
     return (
       <>
-        {" "}
-        {this.state.cv && (
-          <>
-            <SketchBackstretch cv={this.state.cv} />
-            <NavPanel />
-            <SharePanel
-              url={window.location.href}
-              body="Software Engineer Portfolio and Blog"
-              hashtag="DCTechPortfolio"
+        <SketchBackstretch cv={this.state.cv} />
+        <NavPanel />
+        <SharePanel
+          url={window.location.href}
+          body="Software Engineer Portfolio and Blog"
+          hashtag="DCTechPortfolio"
+        />
+        <div className="app">
+          <Switch>
+            <Route exact path="/">
+              <Profile cv={this.state.cv} />
+            </Route>
+            <Route
+              path={"/blog/:id"}
+              children={({
+                match,
+              }: RouteComponentProps<BlogRouteParams>): JSX.Element =>
+                this.state.blog ? (
+                  <BlogPost id={match.params.id} blog={this.state.blog} />
+                ) : (
+                  <></>
+                )
+              }
             />
-            <div className="app">
-              <Switch>
-                <Route exact path="/">
-                  <Profile cv={this.state.cv} />
-                </Route>
-                <Route
-                  path={"/blog/:id"}
-                  children={({
-                    match,
-                  }: RouteComponentProps<BlogRouteParams>): JSX.Element =>
-                    this.state.blog ? (
-                      <BlogPost id={match.params.id} blog={this.state.blog} />
-                    ) : (
-                      <></>
-                    )
-                  }
-                />
-              </Switch>
-              {this.state.blog && <Blog blog={this.state.blog} />}
-              <footer id="footer">
-                <Contact profiles={this.state.cv.basics.profiles} />
-                <Copyright />
-              </footer>
-            </div>
-          </>
-        )}{" "}
+          </Switch>
+          {this.state.blog && <Blog blog={this.state.blog} />}
+          <footer id="footer">
+            <Contact profiles={this.state.cv.basics.profiles} />
+            <Copyright />
+          </footer>
+        </div>
       </>
     );
   }
