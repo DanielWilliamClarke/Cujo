d1d5749c0092bfbe9f5e66dae8fb61cbed392739
DanielWilliamClarke <clarkit@gmail.com>
2022-03-18 16:26:30 +0000

    implemented a quick devicon to react icon override so I can use react icons when a devicon does not exist

--- portfolio/src/components/shared/DevIcon.tsx
+++ portfolio/src/components/shared/DevIcon.tsx
@@ -1,18 +1,47 @@
 import { Component } from "react";
 import { DevIcon } from "../../model/CVModel";
-
+import { IconType } from "react-icons";
+import { FaDeviantart } from "react-icons/fa";
 import "./DevIcon.scss";
 
 type DevIconProps = {
   icon: DevIcon;
+  iconOverride?: IconType;
 };
-export class DevIconName extends Component<DevIconProps> {
+
+type DevIconState = {
+  iconOverride: IconType | undefined;
+};
+
+export class DevIconName extends Component<DevIconProps, DevIconState> {
+  componentDidMount() {
+    this.setState({ iconOverride: this.brandToIcon(this.props.icon) });
+  }
+
   render(): JSX.Element {
+    let iconComponent: JSX.Element;
+    if (this.state?.iconOverride) {
+      iconComponent = <this.state.iconOverride className="icon-override" />;
+    } else {
+      iconComponent = (
+        <span className={`icon devicon-${this.props.icon.icon}`} />
+      );
+    }
+
     return (
       <div className="dev-icon">
-        <span className={`icon devicon-${this.props.icon.icon}`} />
+        {iconComponent}
         <p className="icon-name">{this.props.icon.name}</p>
       </div>
     );
+  }
+
+  private brandToIcon(brand: DevIcon): IconType | undefined {
+    switch (brand.name) {
+      case "DeviantArt":
+        return FaDeviantart;
+      default:
+        return;
+    }
   }
 }
