6283bfdd027be06770babc7afdba6225ee96ce2c
DanielWilliamClarke <clarkit@gmail.com>
2022-04-06 13:30:52 +0100

    refactoring

--- service/src/server/config.rs
+++ service/src/server/config.rs
@@ -4,11 +4,11 @@ use crate::util::FromEnv;
 use contentful::ContentfulConfig;
 use serde::Deserialize;
 
+impl FromEnv for ContentfulConfig {}
+
 #[derive(Deserialize, Debug, Clone)]
 pub struct ServerConfig {
     pub host: String,
     pub port: String,
 }
 impl FromEnv for ServerConfig {}
-
-impl FromEnv for ContentfulConfig {}
