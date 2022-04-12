9bf1b9a0234635eecfec03f482227dfccacda57c
DanielWilliamClarke <clarkit@gmail.com>
2022-04-12 17:41:20 +0100

    Implemented progress gauges to show competency level in skills

---
+++ portfolio/src/components/shared/ProgressGauge.tsx
@@ -0,0 +1,70 @@
+import { Component } from "react";
+import {
+  buildStyles,
+  CircularProgressbarWithChildren,
+} from "react-circular-progressbar";
+import VisibilitySensor from "react-visibility-sensor";
+import { ProgressProvider } from "../shared/ProgressProvider";
+const interpolate = require("color-interpolate");
+
+type ProgressGaugeProps = {
+  value: number;
+  colors: string[];
+  children: (color: string) => JSX.Element;
+};
+
+type ProgressGaugeState = {
+  colormap: any;
+  isVisible: boolean;
+};
+
+export class ProgressGauge extends Component<
+  ProgressGaugeProps,
+  ProgressGaugeState
+> {
+  constructor(props: ProgressGaugeProps) {
+    super(props);
+    this.state = {
+      colormap: interpolate(this.props.colors),
+      isVisible: false,
+    };
+  }
+
+  render(): JSX.Element {
+    return (
+      <VisibilitySensor>
+        {({ isVisible }) => {
+          if (isVisible && !this.state.isVisible) {
+            this.setState({ isVisible });
+          }
+
+          return (
+            <ProgressProvider
+              valueStart={0}
+              valueEnd={this.state.isVisible ? this.props.value : 1}
+            >
+              {(value: number): JSX.Element => {
+                const color = this.state.colormap(value / 100);
+                return (
+                  <CircularProgressbarWithChildren
+                    value={value}
+                    circleRatio={0.75}
+                    styles={buildStyles({
+                      strokeLinecap: "round",
+                      rotation: 1 / 2 + 1 / 8,
+                      trailColor: "#eeeeee99",
+                      pathColor: color,
+                      pathTransitionDuration: 2.0,
+                    })}
+                  >
+                    {this.props.children(color)}
+                  </CircularProgressbarWithChildren>
+                );
+              }}
+            </ProgressProvider>
+          );
+        }}
+      </VisibilitySensor>
+    );
+  }
+}
---
+++ portfolio/src/components/shared/ProgressProvider.tsx
@@ -0,0 +1,48 @@
+import { Component } from "react";
+
+type ProgressProps = {
+  valueStart: number;
+  valueEnd: number;
+  children: (value: number) => JSX.Element;
+};
+
+type ProgressState = {
+  value: number;
+  timeout?: number;
+};
+
+export class ProgressProvider extends Component<ProgressProps, ProgressState> {
+  constructor(props: ProgressProps) {
+    super(props);
+    this.state = {
+      value: this.props.valueStart,
+      timeout: undefined,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      timeout: window.setTimeout(() => {
+        this.setState({
+          value: this.props.valueEnd,
+        });
+      }, 0),
+    });
+  }
+
+  componentDidUpdate(prevProps: ProgressProps, prevState: ProgressState) {
+    if (prevProps.valueEnd !== this.props.valueEnd) {
+      this.setState({
+        value: this.props.valueEnd,
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    window.clearTimeout(this.state.timeout);
+  }
+
+  render(): JSX.Element {
+    return this.props.children(this.state.value);
+  }
+}
