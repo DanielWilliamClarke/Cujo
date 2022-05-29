92bda0c3bfb3ad7171c2943edfc3cc8c671c18a2
DanielWilliamClarke <clarkit@gmail.com>
2022-05-29 20:03:50 +0100

    minor code splitting changes to try to improve performance

--- portfolio/src/index.tsx
+++ portfolio/src/index.tsx
@@ -1,29 +1,83 @@
-import "reflect-metadata";
-
-import React from "react";
+import { Provider } from "inversify-react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "inversify-react";
+
+import "reflect-metadata";
 
 import { container } from "./ioc";
-import App from "./App";
+import { Post } from "./model/BlogPost";
+import { CV } from "./model/CVModel";
+import { Entries } from "./model/Includes";
+import { BlockReverseLoading } from "./components/shared/BlockReverseLoading";
+import { CujoService, ICujoService } from "./services/CujoService";
 
-import "./index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-vertical-timeline-component/style.min.css";
+import "./index.scss";
 
-class Cujo extends React.Component {
+const App = React.lazy(() => import(/* webpackChunkName: "App" */ "./App"));
+
+type CujoProps = {
+  service: ICujoService;
+};
+
+type CujoState = {
+  cv: CV | undefined;
+  blog: Entries<Post> | undefined;
+};
+
+class Cujo extends React.Component<CujoProps, CujoState> {
+  constructor(props: CujoProps) {
+    super(props);
+    this.state = {
+      cv: undefined,
+      blog: undefined,
+    };
+
+    this.props.service.FetchCV().then((cv: CV) => {
+      this.setState({ cv });
+    });
+    this.props.service.FetchBlogPosts().then((blog: Entries<Post>) => {
+      this.setState({ blog });
+    });
+  }
+
   render(): JSX.Element {
+    if (!this.state.cv) {
+      return this.displayLoading();
+    }
+
     return (
       <React.StrictMode>
         <Router>
           <Provider container={container}>
-            <App />
+            <Suspense fallback={<div>Loading...</div>}>
+              <App cv={this.state.cv!} blog={this.state.blog} />
+            </Suspense>
           </Provider>
         </Router>
       </React.StrictMode>
     );
   }
+
+  private displayLoading(): JSX.Element {
+    return (
+      <BlockReverseLoading
+        style={{
+          height: "100vh",
+          width: "auto",
+        }}
+        box={{
+          speed: 3,
+          size: 50,
+        }}
+      />
+    );
+  }
 }
 
-ReactDOM.render(<Cujo />, document.getElementById("root"));
+ReactDOM.render(
+  <Cujo service={new CujoService()} />,
+  document.getElementById("root")
+);
