96154702bc7fd6efeda0bc6656aa8452e6d60ac3
DanielWilliamClarke <clarkit@gmail.com>
2022-05-29 20:03:50 +0100

    minor code splitting changes to try to improve performance

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,4 +1,3 @@
-import { resolve } from "inversify-react";
 import React from "react";
 import { Fade } from "react-awesome-reveal";
 import {
@@ -19,31 +18,20 @@ import { Profile } from "./components/Profile";
 import { Post } from "./model/BlogPost";
 import { CV } from "./model/CVModel";
 import { Entries } from "./model/Includes";
-import { ICujoService } from "./services/CujoService";
 import { BlockReverseLoading } from "./components/shared/BlockReverseLoading";
 
 import "./App.scss";
 
-export type AppState = {
+type AppProps = {
   cv: CV | undefined;
   blog: Entries<Post> | undefined;
 };
 
 type BlogRouteParams = { id: string };
 
-class App extends React.Component<RouteComponentProps, AppState> {
-  @resolve("CujoService") private readonly cujoService!: ICujoService;
-
-  async componentDidMount() {
-    const [cv, blog] = await Promise.all([
-      this.cujoService.FetchCV(),
-      this.cujoService.FetchBlogPosts(),
-    ]);
-    this.setState({ cv, blog });
-  }
-
+class App extends React.Component<AppProps & RouteComponentProps> {
   render(): JSX.Element {
-    return !this.state?.cv ? this.displayLoading() : this.displayApp();
+    return !this.props?.cv ? this.displayLoading() : this.displayApp();
   }
 
   private displayLoading(): JSX.Element {
@@ -65,7 +53,7 @@ class App extends React.Component<RouteComponentProps, AppState> {
     return (
       <>
         <Fade triggerOnce damping={0.01}>
-          <SketchBackstretch cv={this.state.cv!} />
+          <SketchBackstretch cv={this.props.cv!} />
         </Fade>
         <NavPanel />
         <SharePanel
@@ -76,21 +64,21 @@ class App extends React.Component<RouteComponentProps, AppState> {
         <div className="app">
           <Switch>
             <Route exact path="/">
-              <Profile cv={this.state.cv!} />
+              <Profile cv={this.props.cv!} />
             </Route>
             <Route path={"/blog/:id"}>
               {({ match }: RouteComponentProps<BlogRouteParams>): JSX.Element =>
-                this.state.blog ? (
-                  <BlogPost id={match.params.id} blog={this.state.blog} />
+                this.props.blog ? (
+                  <BlogPost id={match.params.id} blog={this.props.blog} />
                 ) : (
                   <></>
                 )
               }
             </Route>
           </Switch>
-          {this.state.blog && <Blog blog={this.state.blog} />}
+          {this.props.blog && <Blog blog={this.props.blog} />}
           <footer id="footer">
-            <Contact profiles={this.state.cv!.about.entry.profiles} />
+            <Contact profiles={this.props.cv!.about.entry.profiles} />
             <Copyright />
           </footer>
         </div>
