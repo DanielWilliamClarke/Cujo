429d8bbc15f939e86d8bd88ce503207071344726
Daniel William Clarke <dan.clarke@loveholidays.com>
2022-07-22 10:14:20 +0100

    remove old cujo service client methods

--- portfolio/src/services/CujoService.ts
+++ portfolio/src/services/CujoService.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { injectable } from "inversify";
 import { GraphQLClient } from 'graphql-request'
 
@@ -11,21 +10,11 @@ import CujoQuery from './Cujo.gql';
 export type GraphQLResponse = {cv: CV, blog: Entries<Post>}
 
 export interface ICujoService {
-  FetchCV(): Promise<CV>
-  FetchBlogPosts(): Promise<Entries<Post>>
   FetchGraphQL(): Promise<GraphQLResponse> 
 }
 
 @injectable()
 export class CujoService implements ICujoService {
-  async FetchCV(): Promise<CV> {
-    return (await axios.get('/api/cv')).data as CV;
-  }
-
-  async FetchBlogPosts(): Promise<Entries<Post>> {
-    return (await axios.get('/api/blog')).data as Entries<Post>;
-  }
-
   async FetchGraphQL(): Promise<GraphQLResponse> {
     return await new GraphQLClient('/api/graphql').request<GraphQLResponse>(CujoQuery);
   }
