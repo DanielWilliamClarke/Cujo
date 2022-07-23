0c900edbb0aed16ae0a6df5103dad59386a226b8
Daniel <clarkit@gmail.com>
2022-07-23 19:23:55 +0100

    nav panel is now fc

--- portfolio/src/components/nav/NavPanel.tsx
+++ portfolio/src/components/nav/NavPanel.tsx
@@ -1,54 +1,51 @@
-import { resolve } from "inversify-react";
-import React from "react";
+import { useInjection } from "inversify-react";
+import React, { useState } from "react";
 import { Nav, Navbar } from "react-bootstrap";
-import { RouteComponentProps, withRouter } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Scrollspy from "react-scrollspy";
 import { IIconService } from "../../services/IconService";
 
 import "./NavPanel.scss";
 
-type NavState = {
-  bg: string | undefined;
-  menu: string[];
-};
+export const NavPanel: React.FC = (): JSX.Element => {
+  const iconService = useInjection(IIconService.$);
+  const [bg, setBg] = useState<string | undefined>(undefined);
+  const location = useLocation();
 
-class NavPanel extends React.Component<RouteComponentProps, NavState> {
-  @resolve(IIconService.$) private readonly iconService!: IIconService;
+  const buildMenuItems = (): string[] => {
+    return location.pathname === "/"
+      ? ["about", "experience", "education", "skills", "projects"]
+      : ["post"];
+  };
 
-  constructor(props: RouteComponentProps) {
-    super(props);
+  window.addEventListener("scroll", () => {
+    setBg(window.scrollY < window.innerHeight ? undefined : "dark");
+  });
 
-    this.state = {
-      bg: undefined,
-      menu: ["home"]
-        .concat(this.buildMenuItems())
-        .concat(["blog", "cv", "profiles"]),
-    };
+  const menu = ["home"]
+    .concat(buildMenuItems())
+    .concat(["blog", "cv", "profiles"]);
 
-    window.addEventListener("scroll", this.listenScrollEvent);
-  }
-
-  render(): JSX.Element {
-    return (
+  return (
       <Navbar
-        bg={this.state.bg}
+        bg={bg}
         sticky="top"
         variant="dark"
         className="justify-content-center"
       >
         <Nav justify navbarScroll style={{ textTransform: "capitalize" }}>
           <Scrollspy
-            items={this.state.menu}
+            items={menu}
             currentClassName="active"
             offset={-100}
             componentTag="nav"
           >
-            {this.state.menu.map((link: string): JSX.Element => {
+            {menu.map((link: string): JSX.Element => {
               const href =
                 link !== "home"
-                  ? `${this.props.location.pathname}#${link}`
+                  ? `${location.pathname}#${link}`
                   : `/#${link}`;
-              const NavIcon = this.iconService.getWithDefault(link);
+              const NavIcon = iconService.getWithDefault(link);
               return (
                 <Nav.Link href={href}>
                   <NavIcon />
@@ -60,19 +57,4 @@ class NavPanel extends React.Component<RouteComponentProps, NavState> {
         </Nav>
       </Navbar>
     );
-  }
-
-  private buildMenuItems(): string[] {
-    return this.props.location.pathname === "/"
-      ? ["about", "experience", "education", "skills", "projects"]
-      : ["post"];
-  }
-
-  private listenScrollEvent = () => {
-    this.setState({
-      bg: window.scrollY < window.innerHeight ? undefined : "dark",
-    });
-  };
-}
-
-export default withRouter(NavPanel);
+};
