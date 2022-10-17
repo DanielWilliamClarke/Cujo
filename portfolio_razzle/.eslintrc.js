cbe31195c437b6c67f805747de907ee75ef8d1ed
Daniel William Clarke <dan.clarke@loveholidays.com>
2022-10-17 20:35:50 +0100

    fixed file properly this time

--- portfolio_razzle/.eslintrc.js
+++ portfolio_razzle/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    project: ['./tsconfig.json']
+    project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname
   },
   plugins: [
